a5da571f6c5c37adf45a8237bd620c9df0a062b7
AceTep <72736849+AceTep@users.noreply.github.com>
2024-02-14 21:27:53 +0100

    Delete bookstar/mysite directory

--- bookstar/mysite/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-model_list = [User_Group, User, Publisher, Author, Book, Category, Comment]
-
-admin.site.register(model_list)
-
-# Register your models here.
